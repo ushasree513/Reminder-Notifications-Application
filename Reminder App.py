3c78c0d4669a9178def4865d0efa9c548495ddce
ushasree513 <ushasree513@gmail.com>
2022-08-16 22:43:06 +0530

    Updated Variable Name

--- Reminder App.py
+++ Reminder App.py
@@ -1,7 +1,7 @@
 import time
 
 print("What you want me to remind you?")
-GetData = str(input())
+Text_Input = str(input())
 GetTime = float(input("Enter time in minutes"))
 GetTime = GetTime * 60
 time.sleep(GetTime)
