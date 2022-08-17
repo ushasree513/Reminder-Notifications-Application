70d4a65fb9ff26c34437e373db3c2bb3d97b0b58
ushasree513 <ushasree513@gmail.com>
2022-08-17 20:33:03 +0530

    Included Notifier

--- Reminder App.py
+++ Reminder App.py
@@ -1,8 +1,27 @@
+from email import message
 import time
+from tkinter import *
+from win10toast import *
 
-print("What you want me to remind you?")
-Text_Input = str(input())
-GetTime = float(input("Enter time in minutes"))
-GetTime = GetTime * 60
-time.sleep(GetTime)
-print(GetData)
+root = Tk()
+root.geometry("800x400")
+l1 = Label(root,font=('Bold',30))
+name_entry = Entry(root, width=30)
+name_entry.pack()
+name_entry.insert(0,"What you want me to remind you?")
+Time_Enter = Entry(root,width=30)
+Time_Enter.pack()
+Time_Enter.insert(0,"Enter Time in Minutes")
+
+def Display():
+    Text_Input = name_entry.get()
+    GetTime = int(Time_Enter.get())
+    GetTime = GetTime * 60
+    time.sleep(GetTime)
+    l1.config(text=Text_Input)
+    l1.pack()
+    hr = ToastNotifier()
+    hr.show_toast("remind",Text_Input)
+
+Display_Label = Button(root,text="Click to Program it",font=('Normal',20),command=Display).pack()
+root.mainloop()
