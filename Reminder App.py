45dbcff787f95fd531285d9edda2ac6cffd87794
ushasree513 <ushasree513@gmail.com>
2022-08-20 16:13:34 +0530

    Set Reminder based on Excel Data

--- Reminder App.py
+++ Reminder App.py
@@ -1,27 +1,20 @@
-from email import message
+
+
+import pandas
 import time
-from tkinter import *
-from win10toast import *
 
-root = Tk()
-root.geometry("800x400")
-l1 = Label(root,font=('Bold',30))
-name_entry = Entry(root, width=30)
-name_entry.pack()
-name_entry.insert(0,"What you want me to remind you?")
-Time_Enter = Entry(root,width=30)
-Time_Enter.pack()
-Time_Enter.insert(0,"Enter Time in Minutes")
+from plyer import *
 
-def Display():
-    Text_Input = name_entry.get()
-    GetTime = int(Time_Enter.get())
-    GetTime = GetTime * 60
-    time.sleep(GetTime)
-    l1.config(text=Text_Input)
-    l1.pack()
-    hr = ToastNotifier()
-    hr.show_toast("remind",Text_Input)
+Map_Data = pandas.read_excel('Reminder.xlsx').to_dict('records')
+for reader in  Map_Data:
+    time.sleep(reader['Seconds'])
+    notification.notify(
+            title = reader['Reminder Title'],
+            message = reader['Reminder Title'] + " at " + str(reader['Time'])
+        )
 
-Display_Label = Button(root,text="Click to Program it",font=('Normal',20),command=Display).pack()
-root.mainloop()
+    
+
+
+    
+
